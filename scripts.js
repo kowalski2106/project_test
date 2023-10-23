8543f371b19acc236cd1764429ebd6ebf3c5c387
Igor <avalan97@gmail.com>
2023-10-23 08:55:15 +0300

    try to calculate

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+let num = 20;
+let firstName = 'Igorek';
+const isProgrammer = true;
+num = 10;
+
+console.log('Number is:', num); 
+console.log(num + 5);
+
+let num2 = num + 10;
+console.log(num2); 
+
+const fullName = firstName + ' Bilenko';
+console.log(fullName);
+
